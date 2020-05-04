060d3d40f0a2108baa84314e18dce5354161b062
TitiAlone <talone@boxph.one>
2020-05-04 11:31:26 +0200

    Add simple documentation renderer

---
+++ Task04/docs/generate.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const promisify = require("util").promisify;
+
+const unified = require("unified");
+const remarkParse = require("remark-parse");
+const remarkRehype = require("remark-rehype");
+const rehypeFormat = require("rehype-format");
+const rehypeDocument = require("rehype-document");
+const rehypeStringify = require("rehype-stringify");
+
+const processor = unified()
+	.use(remarkParse)
+	.use(remarkRehype)
+	.use(rehypeDocument, { title: "Demo doc" })
+	.use(rehypeFormat)
+	.use(rehypeStringify);
+
+const readdir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+const sourceDir = "source";
+const resultDir = "public";
+
+function processAllFiles(files) {
+	return Promise.all(files.map(filename => {
+		const fullPath = `${__dirname}/${sourceDir}/${filename}`;
+		const renderedName =
+			`${__dirname}/${resultDir}/${filename.slice(0, -3)}.html`;
+
+		return readFile(fullPath, { encoding: "utf8" })
+			.then(processor.process)
+			.then(vf => vf.contents)
+			.then(c => writeFile(renderedName, c));
+	}));
+}
+
+function filterMarkdown(readdirResult) {
+	return new Promise(resolve => {
+		resolve(readdirResult
+			.filter(file => {
+				return	(file.indexOf(".") !== 0) &&
+						(file.slice(-3) === ".md");
+			}));
+	});
+}
+
+function generateDoc(dirname) {
+	readdir(`${__dirname}/${dirname}`)
+		.then(filterMarkdown)
+		.then(processAllFiles)
+		.catch(e => console.log(e));
+}
+
+generateDoc(sourceDir);
