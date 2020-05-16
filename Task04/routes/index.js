af938d5b6a8a4ba8c01e197a4401c98ffd00251b
frareb <francois.rebaudo@ird.fr>
2020-05-16 17:21:25 +0200

    simple first web pages

--- Task04/routes/index.js
+++ Task04/routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // render a basic home page
 router.get("/", function(req, res) {
-	res.render("index", { title: "PI2P" });
+	res.render("index", { title: "PI2P", longTitle: "Predicting Insect Pest Phenology" });
+});
+
+// testing simplechart rendering
+router.get("/vizs1", function(req, res) {
+	res.render("simplechart", { title: "PI2P"});
+});
+
+router.get("/vizm1", function(req, res) {
+	res.render("multiplechart", { title: "PI2P", layout: "layout" });
 });
 
 module.exports = router;
