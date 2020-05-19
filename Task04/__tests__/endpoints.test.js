03ae946c5b033d85ce4bb61620292711281d08f1
TitiAlone <talone@boxph.one>
2020-05-19 11:38:27 +0200

    Add authorization tests

--- Task04/__tests__/endpoints.test.js
+++ Task04/__tests__/endpoints.test.js
@@ -1,11 +1,3 @@
-test("dissallow correctly", () => {
-
-});
-
-test("allow correctly", () => {
-
-});
-
-test("works with url parameters", () => {
+test("a test", () => {
 
 });
--- Task04/authorization/authenticator.js
+++ Task04/authorization/authenticator.js
@@ -2,7 +2,7 @@ const models = require("../models");
 const sequelize = models.sequelize;
 
 module.exports = async token => {
-	// Forge custom query to same time
+	// Forge custom query to get group and sensors at the same time
 	const result = await sequelize.query(`
 		SELECT 
 			"Groups"."name" AS "group",
@@ -19,16 +19,16 @@ module.exports = async token => {
 	// Reduce the results as
 	// - groups: a list of groups, discarded by authenticator;
 	// - group: the group to use;
-	// - params: list of authorized url parameters.
+	// - props: list of authorized url parameters.
 	return result.reduce((acc, cur) => {
 		const { group, sensorId } = cur;
 		if(!acc.groups.includes(group)) {
 			acc.group = group;
 			acc.groups.push(group);
 		}
-		if(!acc.params.sensorId.includes(sensorId))
-			acc.params.sensorId.push(sensorId);
+		if(!acc.props.sensorId.includes(sensorId))
+			acc.props.sensorId.push(sensorId);
 
 		return acc;
-	}, {groups: [], group: null, params: {sensorId: []}});
+	}, {groups: [], group: null, props: {sensorId: []}});
 };
