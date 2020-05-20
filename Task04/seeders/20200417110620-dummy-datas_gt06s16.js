93aa173f3a72e718665a19dc0caacfc871b348fa
Francois Rebaudo <francois.rebaudo@ird.fr>
2020-05-20 13:13:43 +0200

    new dummy-datas for testing charts

---
+++ Task04/seeders/20200417110620-dummy-datas_gt06s16.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const axios = require("axios");
+const API_TEMP_URL = "http://pi2p.site/api/b827eb4c2f08/pressure";
+
+module.exports = {
+	up: async (queryInterface) => {
+		// fetch datas from the old API
+		const oldApiRes = await axios.get(API_TEMP_URL);
+
+		const datas = oldApiRes.data.map(e => ({
+			sensorId: 16,
+			value: e.value,
+			createdAt: new Date(e.createdAt - 2*60*60*1000),
+			updatedAt: new Date(e.createdAt - 2*60*60*1000),
+		}));
+
+		return queryInterface.bulkInsert("Datas", datas);
+	},
+	down: (queryInterface) => {
+		return queryInterface.bulkDelete("Datas", null, {});
+	},
+};
