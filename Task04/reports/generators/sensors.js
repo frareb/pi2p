eb2b3c55165f9757194a367af7d6ae4d87bd1c65
TitiAlone <talone@boxph.one>
2020-05-26 11:52:41 +0200

    Construct MDAST on sensors data collection

--- Task04/reports/generators/sensors.js
+++ Task04/reports/generators/sensors.js
@@ -3,34 +3,79 @@ const sequelize = models.sequelize;
 
 const i18n = require("../utils/i18n");
 
-return sequelize.query(`
-	WITH quartiles AS (
-		SELECT
-			NTILE(4) OVER(
-				PARTITION BY EXTRACT(DAY FROM "createdAt")
-				ORDER BY value
-			) AS quart,
-			EXTRACT(DAY FROM "createdAt") as day_of_month,
-			value
-		FROM "Datas"
-		WHERE
-			EXTRACT(MONTH FROM "Datas"."createdAt") = $1
-			AND "Datas"."sensorId" = $2
-	) SELECT
-		MAX(value),
-		MIN(value),
-		quart,
-		day_of_month
-	FROM quartiles
-	GROUP BY quart, day_of_month
-	ORDER BY day_of_month;
-`, {
-	bind: [5, 1],
-	type: sequelize.QueryTypes.SELECT,
-}).then(res => {
-	const data = [];
+const generator = (month, sensor) => {
+	function capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
 
-	res.forEach(e => {
-		
+	return sequelize.query(`
+		WITH quartiles AS (
+			SELECT
+				NTILE(4) OVER(
+					PARTITION BY EXTRACT(DAY FROM "createdAt")
+					ORDER BY value
+				) AS quart,
+				EXTRACT(DAY FROM "createdAt") as day_of_month,
+				value
+			FROM "Datas"
+			WHERE
+				EXTRACT(MONTH FROM "Datas"."createdAt") = $1
+				AND "Datas"."sensorId" = $2
+		) SELECT
+			MAX(value),
+			AVG(value),
+			MIN(value),
+			quart,
+			day_of_month
+		FROM quartiles
+		GROUP BY quart, day_of_month
+		ORDER BY day_of_month, quart;
+	`, {
+		bind: [month, sensor],
+		type: sequelize.QueryTypes.SELECT,
+	}).then(res => {
+		const cellConstructor = value => ({
+			type: "tableCell",
+			children: [{type: "text", value: String(value)}],
+		});
+
+		const mdast = {
+			type: "table",
+			children: [{
+				type: "tableRow",
+				children: [
+					capitalizeFirstLetter(i18n("DAY")),
+					"Min.",
+					i18n("QU_1"),
+					"Med.",
+					"Mean",
+					i18n("QU_3"),
+					"Max.",
+				].map(cellConstructor),
+			}],
+		};
+
+		for(let i = 0; i < res.length; i += 4) {
+			mdast.children.push({
+				type: "tableRow",
+				children: [
+					res[i].day_of_month,
+					res[i].min,
+					res[i].max,
+					res[i + 1].max,
+					res[i].avg,
+					res[i + 2].max,
+					res[i + 3].max,
+				].map(v => cellConstructor(Math.round(v * 100) / 100)),
+			});
+		}
+
+		return mdast;
 	});
-});
+};
+
+module.exports = {
+	name: i18n("SENSORS_MEASUREMENTS_MAIN_TITLE"),
+	order: 20,
+	generator,
+};
--- Task04/reports/utils/i18n.js
+++ Task04/reports/utils/i18n.js
@@ -4,13 +4,19 @@ let lang = "";
 const STRINGS = {
 	"french": {
 		"SENSORS_AVAILABILITY_MAIN_TITLE": "Collecte des données du mois par le serveur",
+		"SENSORS_MEASUREMENTS_MAIN_TITLE": "Récapitulatif des mesures des capteurs",
 		"DAY": "jour",
 		"PERCENT": "pourcentage",
+		"QU_1": "1er Qua.",
+		"QU_3": "3ème Qua.",
 	},
 	"spanish": {
 		"SENSORS_AVAILABILITY_MAIN_TITLE": "Recolección de datos del mes en el servidor",
+		"SENSORS_MEASUREMENTS_MAIN_TITLE": "Recapitulación de las medidas de los sensores",
 		"DAY": "día",
 		"PERCENT": "porcentaje",
+		"QU_1": "1e Cua.",
+		"QU_3": "3e Cua.",
 	},
 };
 
