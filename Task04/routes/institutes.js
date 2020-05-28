f3b3ebd7ae7808f54031c63e846525bd85bb4194
TitiAlone <talone@boxph.one>
2020-05-28 15:30:49 +0200

    Add API endpoint for generating reports

--- Task04/routes/institutes.js
+++ Task04/routes/institutes.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const model = require("../models").Institutes;
+const report = require("../reports");
 
 require("./factory")({
 	model,
 	router,
+});
+
+router.get("/:instituteId/report", (req, res) => {
+	const month = req.query.month || new Date().getMonth();
+	const lang = req.query.lang || "french";
+
+	report({
+		lang,
+		month,
+		institute: req.params.instituteId,
+	}).then(tex => {
+		res.header("Content-Type", "application/x-tex");
+		return res.status(200).send(tex);
+	}).catch(error => res.status(500).json({meta: {error}}));
 });
 
 module.exports = router;
