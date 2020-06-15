35a07d81a11877c2d5ef2d419dd5a72fb650e3f2
Francois Rebaudo <francois.rebaudo@ird.fr>
2020-06-15 13:33:08 +0200

    get reports links

--- Task04/routes/reports.js
+++ Task04/routes/reports.js
@@ -7,17 +7,40 @@ const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
 const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
 const myMonth = `${mo}-${ye}`;
 
-// const testFolder = './files/';
-// const fs = require('fs');
-// fs.readdirSync(testFolder).forEach(file => {
-//   console.log(file);
+const fileFolder = './public/files/';
+const fs = require('fs');
+// const myFiles = [];
+// fs.readdirSync(fileFolder).forEach(file => {
+// 	let inst = file.split('_')[1];
+// 	let lang = file.split('_')[2].split(".")[0];
+// 	let ext = file.split('_')[2].split(".")[1];
+// 	let push = (lang == "french" && ext == "tex") ? myFiles.push(inst) : false;
 // });
+
+const myFiles = [];
+fs.readdirSync(fileFolder).forEach(file => {
+	let inst = file.split('_')[1];
+	let lang = file.split('_')[2].split(".")[0];
+	let ext = file.split('_')[2].split(".")[1];
+	if (lang == "english" && ext == "tex") {
+		myFiles.push({name:inst, tex:[], pdf:[]});
+	}
+	if(ext == "tex"){
+		myFiles[myFiles.length - 1].tex.push(file);
+	}else{
+		if(ext == "pdf"){
+			myFiles[myFiles.length - 1].pdf.push(file);
+		}
+	}
+});
+// console.log(myFiles);
 
 // page to download reports
 router.get("/reports/main", function(req, res) {
 	res.render("reports/reports", {
 		title: "PI2P",
 		myMonth: myMonth,
+		myFiles: myFiles,
 	});
 });
 
