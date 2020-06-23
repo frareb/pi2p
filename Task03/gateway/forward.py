5d7ccf21aea38dcf2ea9569c6846e6a1b70c0e86
TitiAlone <talone@boxph.one>
2020-06-23 14:54:42 +0200

    Add DHT11 & store unacknowledged datas

--- Task03/gateway/forward.py
+++ Task03/gateway/forward.py
@@ -13,18 +13,28 @@ RESET = digitalio.DigitalInOut(board.D6)
 BASE_URL = "http://192.168.2.107:3000/sensors/"
 GW_KEY = "firstGatewayKey"
 SENSOR_MAP = {
-        b"T": 1
+        b"T": 1,
+        b"H": 2,
 }
 
+# Configure RFM95
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+rfm9x.enable_crc = True
+
+# Forge ACK packet
+ack_pkt = bytes(struct.pack("<cfx", b"A", 0x0000))
 
 while True:
     packet = rfm9x.receive(with_header=True)
 
     if packet is not None:
+        # Extract data from packet
         (s_type, s_val) = struct.unpack("<cfx", packet[4:])
-        print(s_type)
+        print(s_type, s_val)
+        # Send ACK to sensor to prevent local data storage
+        rfm9x.send(ack_pkt, keep_listening=True)
+        # Send data to the server
         endpoint = BASE_URL + str(SENSOR_MAP[s_type]) + "/datas"
         headers = { "Authorization": "Bearer " + GW_KEY }
         req = requests.post(endpoint, headers = headers, json = { "value": s_val, "createdAt": round(time.time() * 1000) })
--- Task03/rf95_arduino/rf95_arduino.ino
+++ Task03/rf95_arduino/rf95_arduino.ino
@@ -1,6 +1,6 @@
 /*
- * Program that read a value in °C from DS18B20, sends it using RFM95
- * and then goes to sleep for a four seconds, and sends a new read.
+ * Program that read values from sensors, sends them using RFM95
+ * and then goes to sleep for four seconds, and sends a new read.
  * 
  * Pinout:
  * 
@@ -14,16 +14,21 @@
  *   DIO0 ---> D3
  *   RESET --> D6
  * 
- * | DS18B20 | Arduino |
+ * | Sensor | Arduino |
  *    3.3V ----> 3V3 --|
  *    Gnd -----> Gnd   | 10k resistor from Vcc to DATA
- *    DATA ----> D2 ---|
+ *    DS18B20 -> D2 ---|
+ *    DHT11 ---> D7
  */
 
 #include <SPI.h>
+#include <EEPROM.h>
 #include <RH_RF95.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
@@ -33,10 +38,17 @@
 #define RFM95_CS  4
 #define RFM95_RST 6
 #define TEMP_PIN  2
+#define DHT_PIN   7
+
+#define EE_COUNT_ADDR 0x00
+#define EE_STORE_ADDR 0x01
 
 // Setup oneWire instance & DallasTemperature library
 OneWire oneWire(TEMP_PIN);  
-DallasTemperature temp_sensor(&oneWire);
+DallasTemperature ds18b20(&oneWire);
+
+// Singleton instance of the DHT11 sensor
+DHT_Unified dht(DHT_PIN, DHT11);
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
@@ -61,10 +73,14 @@ void setup() {
   Serial.begin(9600);
   while (!Serial); // Wait for serial port to be available
 
-  // Init DS18B20
-  temp_sensor.begin();
+  // Init sensors
+  ds18b20.begin();
+  dht.begin();
 
   // Perform chip reset
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(100);
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
@@ -83,18 +99,47 @@ void setup() {
 }
 
 void loop() {
-  // Get temperature and forge packet
-  temp_sensor.requestTemperatures();
+  // Read sensors values
+  sensors_event_t humidity_results;
+  ds18b20.requestTemperatures();
+  dht.humidity().getEvent(&humidity_results);
 
+  // Forge and send packets
   RadioPacket pkt;
   pkt.hi.type = 'T';
-  pkt.hi.value = temp_sensor.getTempCByIndex(0);
+  pkt.hi.value = ds18b20.getTempCByIndex(0);
+  pkt_send_with_queue(&pkt);
 
+  // Short sleep before packet resend
+  delay(500);
+  sleep_enter(1);
+
+  pkt.hi.type = 'H';
+  pkt.hi.value = humidity_results.relative_humidity;
+  pkt_send_with_queue(&pkt);
+
+  // Sleep for a minute
+  sleep_enter(1);
+}
+
+void pkt_send_with_queue(RadioPacket *pkt) {
+  // Send current packet
+  if(pkt_send(pkt)) {
+    // Try to resend failing packets
+    stored_pkts_send();
+  } else {
+    // If fail, append packet to queue
+    stored_pkts_append(pkt);
+  }
+}
+
+bool pkt_send(RadioPacket *pkt) {
   // Send the packet  
-  if(!rf95.send((uint8_t *) pkt.raw, sizeof(pkt.raw))) {
+  if(!rf95.send((uint8_t *) pkt->raw, sizeof(pkt->raw))) {
     Serial.println("unable to send message");
     while(1);
   }
+  
   rf95.waitPacketSent();
 
   // Wait for a reply
@@ -103,12 +148,45 @@ void loop() {
   
   if(rf95.waitAvailableTimeout(1000)) {
     if(rf95.recv(rcv_buf, &rcv_len)) {
-      Serial.print("got reply!");
+      return true;
     }
   }
 
-  // Sleep for a minute
-  sleep_enter(7);
+  // No reply: send failure
+  return false;
+}
+
+void stored_pkts_send(void) {
+  // Get current packet count
+  unsigned char remaining_pkts = EEPROM.read(EE_COUNT_ADDR);
+  
+  RadioPacket current_pkt;
+
+  while(remaining_pkts > 0) {
+    remaining_pkts--;
+
+    // Reassemble the six data packet bytes
+    for(char frag = 0; frag < 6; frag++) {
+      current_pkt.raw[frag] = EEPROM.read(EE_STORE_ADDR + 6 * remaining_pkts + frag);
+    }
+
+    // Try to send packet
+    if(!pkt_send(&current_pkt)) break;
+    EEPROM.write(EE_COUNT_ADDR, remaining_pkts);
+  }
+}
+
+void stored_pkts_append(RadioPacket *pkt) {
+  // Get current packet count
+  unsigned char queue_pkts = EEPROM.read(EE_COUNT_ADDR);
+
+  // Dissassemble the data packet bytes
+  for(char frag = 0; frag < 6; frag++) {
+    EEPROM.write(EE_STORE_ADDR + 6 * queue_pkts + frag, pkt->raw[frag]);
+  }
+
+  // Increment the packet queue count
+  EEPROM.write(EE_COUNT_ADDR, queue_pkts + 1);
 }
 
 void sleep_disable_wdt(void) {
@@ -136,10 +214,16 @@ void sleep_enable_wdt(void) {
 }
 
 void sleep_enter(unsigned char cycles) {
+  if(!rf95.sleep()) {
+    Serial.println("could not go to sleep");
+    while(1);
+  }
+
+  // Do not remove this delay or sleep will not work
+  delay(100);
+  
   sleep_cycles = 0;
   sleep_enable_wdt();
-
-  rf95.sleep();
 
   // Go to sleep
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
