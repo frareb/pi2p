1fba0c815ef4e14d9a94c22f87102c73ec8f591e
TitiAlone <talone@boxph.one>
2020-06-30 10:20:06 +0200

    Update deploy script for production

--- Task04/bin/deploy.sh
+++ Task04/bin/deploy.sh
@@ -6,14 +6,15 @@ GIT_COMMIT=$1
 API_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
 # Shut down PI2P API before update
-systemctl stop pi2p-api
+systemctl stop pi2p
 
 # Checkout the requested commit
 cd ${API_DIR}
+git checkout .
 git pull origin master
 git checkout ${GIT_COMMIT}
 
-npm ci --only=production
+npm ci --only=prod
 
 # Restart the API service
-systemctl start pi2p-api
+systemctl start pi2p
