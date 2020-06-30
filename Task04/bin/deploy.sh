41f20bcc66c051c777dfb46a6001f6dd01be35aa
TitiAlone <talone@boxph.one>
2020-06-30 15:15:36 +0200

    [hotfix] Update deploy script

--- Task04/bin/deploy.sh
+++ Task04/bin/deploy.sh
@@ -5,9 +5,6 @@ GIT_COMMIT=$1
 # Path to the Node API directory
 API_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
-# Shut down PI2P API before update
-systemctl stop pi2p
-
 # Checkout the requested commit
 cd ${API_DIR}
 git checkout .
@@ -17,4 +14,4 @@ git checkout ${GIT_COMMIT}
 npm ci --only=prod
 
 # Restart the API service
-systemctl start pi2p
+systemctl restart pi2p
