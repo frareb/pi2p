5df64eaf9df2c4b520af93e14045909f67e4b22c
Francois Rebaudo <francois.rebaudo@ird.fr>
2020-07-02 11:46:54 +0200

    only tex and pdf files in download reports

--- Task04/controllers/reports.js
+++ Task04/controllers/reports.js
@@ -13,18 +13,20 @@ module.exports = () => {
 	const reportFilepaths = [];
 
 	fs.readdirSync(fileFolder).forEach(file => {
-		let inst = file.split("_")[1];
-		let lang = file.split("_")[2].split(".")[0];
-		let ext = file.split("_")[2].split(".")[1];
+		if (file.split('.').pop() == "tex" || file.split('.').pop() == "pdf") {
+			let inst = file.split("_")[1];
+			let lang = file.split("_")[2].split(".")[0];
+			let ext = file.split("_")[2].split(".")[1];
 
-		if(lang == "english" && ext == "pdf") {
-			reportFilepaths.push({name:inst, tex:[], pdf:[]});
-		}
+			if(lang == "english" && ext == "pdf") {
+				reportFilepaths.push({name:inst, tex:[], pdf:[]});
+			}
 
-		if(ext == "tex"){
-			reportFilepaths[reportFilepaths.length - 1].tex.push(file);
-		} else if(ext == "pdf") {
-			reportFilepaths[reportFilepaths.length - 1].pdf.push(file);
+			if(ext == "tex"){
+				reportFilepaths[reportFilepaths.length - 1].tex.push(file);
+			} else if(ext == "pdf") {
+				reportFilepaths[reportFilepaths.length - 1].pdf.push(file);
+			}
 		}
 	});
 
