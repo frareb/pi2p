44fc0e2773979d2faf848d3867c3647607e014f8
TitiAlone <talone@boxph.one>
2020-07-03 09:56:34 +0200

    Webpack chunk optimizations

--- Task04/webpack.config.js
+++ Task04/webpack.config.js
@@ -2,33 +2,46 @@ const path = require("path");
 
 const mode = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
 
-const defaultConf = {
-	mode,
-	module: {
-		rules: [{
-			test: /node_modules/,
-			use: ["ify-loader"],
-		}, {
-			test: /\.js/,
-			exclude: [/node_modules/],
-			use: ["babel-loader"],
-		}],
+const entry = {
+	filterSelectBoxInstGateCap: "./client/filterSelectBoxInstGateCap.js",
+	filterSelectBoxMult: "./client/filterSelectBoxMult.js",
+	comPublicationsTeam: "./client/comPublicationsTeam.js",
+	filterSelectBoxAll: "./client/filterSelectBoxAll.js",
+	visNetworkClimate: "./client/visNetworkClimate.js",
+	homePageAnimation: "./client/homePageAnimation.js",
+};
+
+const optimization = {
+	splitChunks: {
+		chunks: "all",
+		automaticNameDelimiter: "~",
+		cacheGroups: {
+			defaultVendors: {
+				test: /[\\/]node_modules[\\/]/,
+				priority: 1,
+			},
+		},
 	},
 };
 
-const makeExportObject = file => Object.assign({}, defaultConf, {
-	entry: `./client/${file}.js`,
+const moduleConfig = {
+	rules: [{
+		test: /node_modules/,
+		use: ["ify-loader"],
+	}, {
+		test: /\.js/,
+		exclude: [/node_modules/],
+		use: ["babel-loader"],
+	}],
+};
+
+module.exports = {
+	mode,
+	entry,
+	module: moduleConfig,
 	output: {
-		filename: `${file}.js`,
+		filename: "[name].js",
 		path: path.resolve(__dirname, "public/js"),
 	},
-});
-
-module.exports = [
-	"filterSelectBoxAll",
-	"filterSelectBoxInstGateCap",
-	"filterSelectBoxMult",
-	"visNetworkClimate",
-	"comPublicationsTeam",
-	"homePageAnimation",
-].map(makeExportObject);
+	optimization,
+};
